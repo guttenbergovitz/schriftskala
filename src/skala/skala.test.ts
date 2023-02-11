aca313aef4b8430f51886c1a30e9eee445028fb7
guttenbergovitz <guttenbergovitz@gmail.com>
2023-02-11 21:55:16 +0100

    feat: add up() calculation with some tests

--- src/skala/skala.test.ts
+++ src/skala/skala.test.ts
@@ -1,13 +1,39 @@
 import { test, it, expect, describe } from 'vitest'
 import Skala from './skala'
 
-describe('skala', () => {
-  const skala = new Skala(16, 1.5, 'px')
-  it('should have a base font size', () => {
-    expect(skala.baseFontSize).toBeDefined()
+describe('skala 10 | 2', () => {
+  let skala = new Skala(10, 2)
+  test('up 1', () => {
+    expect(skala.up(1).fontSize).toEqual(20)
   })
-  it('should have a base line height', () => {
-    expect(skala.baseLineHeight).toBeDefined()
+  test('up 2', () => {
+    expect(skala.up(2).fontSize).toEqual(40)
+  })
+  test('up 3', () => {
+    expect(skala.up(3).fontSize).toEqual(80)
   })
 })
+
+describe('skala 10 | 1.5', () => {
+  let skala = new Skala(10, 1.5)
+  test('up 1', () => {
+    expect(skala.up(1).fontSize).toEqual(15)
+  })
+  test('up 2', () => {
+    expect(skala.up(2).fontSize).toEqual(22.5)
+  })
+  test('up 3', () => {
+    expect(skala.up(3).fontSize).toEqual(33.75)
+  })
+  test('up 4', () => {
+    expect(skala.up(4).fontSize).toEqual(50.625)
+  })
+  test('up 5', () => {
+    expect(skala.up(5).fontSize).toEqual(75.9375)
+  })
+  test('up 6', () => {
+    expect(skala.up(6).fontSize).toEqual(113.90625)
+  })
+})
+
 
--- src/skala/skala.ts
+++ src/skala/skala.ts
@@ -1,12 +1,32 @@
 class Skala {
   constructor(
     private _baseFontSize: BaseFontSize,
-    private _baseLineHeight: BaseLineHeight,
-    private _unit: Unit,
+    private _multiplier: number = 1.25,
+    private _baseLineHeight: BaseLineHeight = 1.33,
+    private _unit: Unit =  'px',
   ) {}
 
   public get baseFontSize() {
     return this._baseFontSize;
+  }
+
+  public up(n: number): {
+    fontSize: number;
+    lineHeight:  number;
+  } {
+    if (n === 0) {
+      return {
+        fontSize: this._baseFontSize,
+        lineHeight: this._baseLineHeight,
+      };
+    }
+
+    const { fontSize, lineHeight } = this.up(n - 1);
+
+    return {
+      fontSize: fontSize * this._multiplier,
+      lineHeight: lineHeight * this._multiplier,
+    };
   }
 }
 
@@ -15,4 +35,8 @@ type BaseLineHeight = number;
 type Unit = 'px' | 'em' | 'rem';
 
 export default Skala;
+
+function roundToTwoDecimalPlaces(number: number) {
+  return Math.round((number + Number.EPSILON) * 100) / 100;
+}
 
